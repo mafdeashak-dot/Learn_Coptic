9fdd5db90458954def6e321b4cd11e34f2e091aa
mafdeashak-dot <mafdeashak@gmail.com>
2026-08-12 18:25:01 +0300

    Update function.js

--- js/function.js
+++ js/function.js
@@ -1,4 +1,4 @@
-let scrollUp = document.querySelector(".scroll_up");
+/*let scrollUp = document.querySelector(".scroll_up");
 window.addEventListener("scroll", function () {
   if(window.scrollY > 200){
     scrollUp.style.display = "block";
@@ -36,4 +36,77 @@ searchInput.addEventListener("input", function () {
     });
 
     noResults.style.display = found ? "none" : "block";
+});*/
+
+let scrollUp = document.querySelector(".scroll_up");
+window.addEventListener("scroll", function () {
+  if(window.scrollY > 200){
+    scrollUp.style.display = "block";
+  } else {
+    scrollUp.style.display = "none";
+  }
+});
+
+scrollUp.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
+
+
+const searchInput = document.getElementById("searchInput");
+const exerciseCards = document.querySelectorAll(".exercise-card , .exam-card");
+const noResults = document.getElementById("noResults");
+
+if (searchInput) {
+
+    searchInput.addEventListener("input", function () {
+
+        const searchValue = this.value.trim().toLowerCase();
+        let found = false;
+
+        exerciseCards.forEach(card => {
+            const text = card.textContent.toLowerCase();
+
+            if (text.includes(searchValue)) {
+                card.style.display = "flex";
+                found = true;
+            } else {
+                card.style.display = "none";
+            }
+        });
+
+        if (noResults) {
+            noResults.style.display = found ? "none" : "block";
+        }
+
+    });
+
+}
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const elements = document.querySelectorAll(".from-right, .from-left");
+    const observer = new IntersectionObserver((entries) => {
+
+        entries.forEach(entry => {
+
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+                observer.unobserve(entry.target);
+            }
+
+        });
+
+    }, {
+        threshold: 0.2
+    });
+
+    elements.forEach(element => {
+        observer.observe(element);
+    });
+
 });
